48d7f11aeaf60a2bf36890a664c1608170654c1e
Kurt Harriger <kurtharriger@gmial.com>
2012-01-07 15:02:42 -0700

    new project

---
+++ src/lein_groovyc/core.clj
@@ -0,0 +1,1 @@
+(ns lein-groovyc.core)
---
+++ test/lein_groovyc/test/core.clj
@@ -0,0 +1,6 @@
+(ns lein-groovyc.test.core
+  (:use [lein-groovyc.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
